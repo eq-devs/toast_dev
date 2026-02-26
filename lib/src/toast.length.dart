9940ed936bf683203b9fab669289137d5c86ab8c
Ekmadish <estaykargabay1206@gmail.com>
2026-02-26 15:45:08 +0500

    version 1.0.4

--- lib/src/toast.length.dart
+++ lib/src/toast.length.dart
@@ -1,12 +1,25 @@
 enum ToastLength {
+  ultraShort(Duration(milliseconds: 1200)),
+
+  // 🟢
   short(Duration(milliseconds: 2000)),
 
+  // 🟡
   medium(Duration(milliseconds: 3500)),
 
+  // 🔵
   long(Duration(milliseconds: 5000)),
 
+  // 🟣
+  extended(Duration(seconds: 10)),
+
+  // 🟠
+  sticky(Duration(minutes: 5)),
+
+  // 🔴
   ages(Duration(minutes: 2)),
 
+  // ♾
   never(Duration(hours: 24));
 
   const ToastLength(this.duration);
